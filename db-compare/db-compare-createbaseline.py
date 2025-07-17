a969706301d02d4178bfadae27281a082e505a49
Guido Schmutz <schmutz68@gmail.com>
2025-07-17 08:57:12 +0200

    update

--- db-compare/db-compare-createbaseline.py
+++ db-compare/db-compare-createbaseline.py
@@ -1,6 +1,7 @@
 import pytest
 import os
 from sqlalchemy import create_engine, text, inspect
+import json
 
 
 # Read connection details from environment variables
@@ -10,20 +11,11 @@ SRC_HOST = os.getenv('SRC_DB_HOST', 'localhost')
 SRC_PORT = os.getenv('SRC_DB_PORT', '5442')
 SRC_DBNAME = os.getenv('SRC_DB_NAME', 'metastore_db')
 
-TGT_USER = os.getenv('TGT_DB_USER', 'hive')
-TGT_PASSWORD = os.getenv('TGT_DB_PASSWORD', 'abc123!')
-TGT_HOST = os.getenv('TGT_DB_HOST', 'localhost')
-TGT_PORT = os.getenv('TGT_DB_PORT', '5442')
-TGT_DBNAME = os.getenv('TGT_DB_NAME', 'metastore_db')
-
 # Construct connection URLs
 src_url = f'postgresql://{SRC_USER}:{SRC_PASSWORD}@{SRC_HOST}:{SRC_PORT}/{SRC_DBNAME}'
-tgt_url = f'postgresql://{TGT_USER}:{TGT_PASSWORD}@{TGT_HOST}:{TGT_PORT}/{TGT_DBNAME}'
-#tgt_url = f'duckdb:///md:my_db'
 
 # Setup connections
 src_engine = create_engine(src_url)
-tgt_engine = create_engine(tgt_url)
 
 def get_table_names():
     with src_engine.connect() as conn:
@@ -34,18 +26,6 @@ def get_table_names():
               AND table_type = 'BASE TABLE'
         """))
         return [row[0] for row in result]
-
-def get_pk_columns(table):
-    with src_engine.connect() as conn:
-        result = conn.execute(text("""
-            SELECT a.attname AS column_name
-            FROM pg_index i
-            JOIN pg_attribute a ON a.attrelid = i.indrelid AND a.attnum = ANY(i.indkey)
-            WHERE i.indrelid = 'public."{table}}"'::regclass
-            AND i.indisprimary;
-        """))
-        return [row[0] for row in result]
-
 
 def quote_ident(name: str, dialect):
     return dialect.identifier_preparer.quote(name)
@@ -79,27 +59,30 @@ def generate_fingerprint(engine, table: str, schema: str = "public") -> str:
 # Dynamically get table names from the source DB
 tables = get_table_names()
 
-@pytest.mark.parametrize("table", tables)
-def test_row_counts(table):
+def save_table_counts():
+    src_count = {}
     with src_engine.connect() as conn:
-        src_count = conn.execute(text(f'SELECT COUNT(*) FROM "{table}"')).scalar()
+        for table in tables:
+            src_count[table] = conn.execute(text(f'SELECT COUNT(*) FROM "{table}"')).scalar()
     
-    with tgt_engine.connect() as conn:
-        tgt_count = conn.execute(text(f'SELECT COUNT(*) FROM "{table}"')).scalar()
-    
-    assert src_count == tgt_count, f"Mismatch in table '{table}': {src_count} != {tgt_count}"
+    with open('counts.json', 'w') as f:
+        json.dump(src_count, f, indent=4)
 
-@pytest.mark.parametrize("table", tables)
-def test_value_compare(table):
-    if (table == 'COMPACTION_METRICS_CACHE' or table == 'WRITE_SET' or table == 'TXN_TO_WRITE_ID' or table == 'NEXT_WRITE_ID' 
-        or table == 'MIN_HISTORY_WRITE_ID' or table == 'TXN_COMPONENTS' or table == 'COMPLETED_TXN_COMPONENTS' or table == 'TXN_LOCK_TBL'
-        or table == 'NEXT_LOCK_ID' or table == 'NEXT_COMPACTION_QUEUE_ID' ):
-        assert True
-    else:
-        with src_engine.connect() as conn:
-            src_fingerprint: str = generate_fingerprint(src_engine, table, "public")
+def save_table_fingerprints():
+    src_fingerprint = {}
+    tables = get_table_names()
 
-        with tgt_engine.connect() as conn:
-            tgt_fingerprint: str = generate_fingerprint(tgt_engine, table, "public")
+    for table in tables:
+        if (table == 'COMPACTION_METRICS_CACHE' or table == 'WRITE_SET' or table == 'TXN_TO_WRITE_ID' or table == 'NEXT_WRITE_ID' 
+            or table == 'MIN_HISTORY_WRITE_ID' or table == 'TXN_COMPONENTS' or table == 'COMPLETED_TXN_COMPONENTS' or table == 'TXN_LOCK_TBL'
+            or table == 'NEXT_LOCK_ID' or table == 'NEXT_COMPACTION_QUEUE_ID' ):
+            continue
+        else:
+            with src_engine.connect() as conn:
+                src_fingerprint[table] = generate_fingerprint(src_engine, table, "public")
 
-            assert src_fingerprint == tgt_fingerprint, f"Mismatch in table '{table}': {src_fingerprint} != {tgt_fingerprint}"
+    with open('fingerprints.json', 'w') as f:
+        json.dump(src_fingerprint, f, indent=4)
+
+save_table_fingerprints()
+save_table_counts()
--- db-compare/db-compare-to-baseline.py
+++ db-compare/db-compare-to-baseline.py
@@ -1,14 +1,7 @@
 import pytest
 import os
+import json
 from sqlalchemy import create_engine, text, inspect
-
-
-# Read connection details from environment variables
-SRC_USER = os.getenv('SRC_DB_USER', 'hive')
-SRC_PASSWORD = os.getenv('SRC_DB_PASSWORD', 'abc123!')
-SRC_HOST = os.getenv('SRC_DB_HOST', 'localhost')
-SRC_PORT = os.getenv('SRC_DB_PORT', '5442')
-SRC_DBNAME = os.getenv('SRC_DB_NAME', 'metastore_db')
 
 TGT_USER = os.getenv('TGT_DB_USER', 'hive')
 TGT_PASSWORD = os.getenv('TGT_DB_PASSWORD', 'abc123!')
@@ -17,16 +10,16 @@ TGT_PORT = os.getenv('TGT_DB_PORT', '5442')
 TGT_DBNAME = os.getenv('TGT_DB_NAME', 'metastore_db')
 
 # Construct connection URLs
-src_url = f'postgresql://{SRC_USER}:{SRC_PASSWORD}@{SRC_HOST}:{SRC_PORT}/{SRC_DBNAME}'
 tgt_url = f'postgresql://{TGT_USER}:{TGT_PASSWORD}@{TGT_HOST}:{TGT_PORT}/{TGT_DBNAME}'
-#tgt_url = f'duckdb:///md:my_db'
 
 # Setup connections
-src_engine = create_engine(src_url)
 tgt_engine = create_engine(tgt_url)
 
+def quote_ident(name: str, dialect):
+    return dialect.identifier_preparer.quote(name)
+
 def get_table_names():
-    with src_engine.connect() as conn:
+    with tgt_engine.connect() as conn:
         result = conn.execute(text("""
             SELECT table_name 
             FROM information_schema.tables 
@@ -35,20 +28,9 @@ def get_table_names():
         """))
         return [row[0] for row in result]
 
-def get_pk_columns(table):
-    with src_engine.connect() as conn:
-        result = conn.execute(text("""
-            SELECT a.attname AS column_name
-            FROM pg_index i
-            JOIN pg_attribute a ON a.attrelid = i.indrelid AND a.attnum = ANY(i.indkey)
-            WHERE i.indrelid = 'public."{table}}"'::regclass
-            AND i.indisprimary;
-        """))
-        return [row[0] for row in result]
+# Dynamically get table names from the source DB
+tables = get_table_names()
 
-
-def quote_ident(name: str, dialect):
-    return dialect.identifier_preparer.quote(name)
 
 def generate_fingerprint(engine, table: str, schema: str = "public") -> str:
     with engine.connect() as conn:
@@ -76,18 +58,19 @@ def generate_fingerprint(engine, table: str, schema: str = "public") -> str:
             result = conn.execute(query).scalar()
         return result
 
-# Dynamically get table names from the source DB
-tables = get_table_names()
+with open("counts.json", "r") as f:
+    src_counts = json.load(f)
+
+with open("fingerprints.json", "r") as f:
+    src_fingerprints = json.load(f)
 
 @pytest.mark.parametrize("table", tables)
 def test_row_counts(table):
-    with src_engine.connect() as conn:
-        src_count = conn.execute(text(f'SELECT COUNT(*) FROM "{table}"')).scalar()
     
     with tgt_engine.connect() as conn:
         tgt_count = conn.execute(text(f'SELECT COUNT(*) FROM "{table}"')).scalar()
     
-    assert src_count == tgt_count, f"Mismatch in table '{table}': {src_count} != {tgt_count}"
+    assert src_counts[table] == tgt_count, f"Mismatch in table '{table}': {src_counts[table]} != {tgt_count}"
 
 @pytest.mark.parametrize("table", tables)
 def test_value_compare(table):
@@ -96,10 +79,7 @@ def test_value_compare(table):
         or table == 'NEXT_LOCK_ID' or table == 'NEXT_COMPACTION_QUEUE_ID' ):
         assert True
     else:
-        with src_engine.connect() as conn:
-            src_fingerprint: str = generate_fingerprint(src_engine, table, "public")
-
         with tgt_engine.connect() as conn:
             tgt_fingerprint: str = generate_fingerprint(tgt_engine, table, "public")
 
-            assert src_fingerprint == tgt_fingerprint, f"Mismatch in table '{table}': {src_fingerprint} != {tgt_fingerprint}"
+            assert src_fingerprints[table] == tgt_fingerprint, f"Mismatch in table '{table}': {src_fingerprints[table]} != {tgt_fingerprint}"
